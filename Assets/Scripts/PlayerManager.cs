200252a0e10568453f364d315cd330cf4ad43e00
reillydonovan <reillydonovan3@gmail.com>
2018-09-22 14:07:14 -0700

    added player cycling while waiting for users

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+//using HoloToolkit.Unity.SpatialMapping;
+
+public class PlayerManager : MonoBehaviour
+{
+  
+    public GameObject[] playerArray = new GameObject[6];
+    public GameObject KinectController;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B)) nextObject();
+
+        if (KinectController.GetComponent<KinectManager>().calibrationText.text == "WAITING FOR USERS") nextObject();
+        
+    }
+
+    public void nextObject()
+    {
+        for (int i = 0; i < playerArray.Length; i++)
+        {
+            //   if (activateNext > playerArray.Length - 1) activateNext = 0;
+            
+                playerArray[i].GetComponent<AvatarController>().playerIndex += 1;
+                playerArray[i].GetComponent<AvatarScaler>().playerIndex += 1;
+
+            if (playerArray[i].GetComponent<AvatarController>().playerIndex > playerArray.Length -1)
+            {
+                playerArray[i].GetComponent<AvatarController>().playerIndex = 0;
+                playerArray[i].GetComponent<AvatarScaler>().playerIndex = 0;
+
+            }
+        }
+    }
+}
+
+
+
+ 
